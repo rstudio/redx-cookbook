9f7969e67fb77849fd7b1a8599aa85365a331b1e
Chad Barraford <cbarraford@gmail.com>
2014-07-25 15:06:31 -0400

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chad@rstudio.com'
 license 'All rights reserved'
 description 'Installs/Configures redx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '1.0.0'
 
 depends 'redx'
 depends 'git'
